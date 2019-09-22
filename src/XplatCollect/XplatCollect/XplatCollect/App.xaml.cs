167274046a4843249198be6f3368fc0a6689d3ad
ThaisCristinaoa <52842898+ThaisCristinaoa@users.noreply.github.com>
2019-09-22 12:46:51 -0300

    versão 1

---
+++ src/XplatCollect/XplatCollect/XplatCollect/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Xamarin.Forms;
+using XplatCollect.ViewModels;
+using XplatCollect.Views;
+
+namespace XplatCollect
+{
+    public partial class App : PrismApplication
+    {
+        public App() : this(null)
+        {
+        }
+
+        public App(IPlatformInitializer platformInitializer)
+            : base(platformInitializer, true)
+        {
+        }
+
+        public App(IPlatformInitializer platformInitializer, bool setFormsDependencyResolver)
+            : base(platformInitializer, setFormsDependencyResolver)
+        {
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+            containerRegistry.RegisterForNavigation<ProfilePage>();
+            containerRegistry.RegisterForNavigation<NewCollectionPage>();
+            containerRegistry.RegisterForNavigation<CollectionPage>();
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService
+                .NavigateAsync($"{nameof(NavigationPage)}/{nameof(MainPage)}?selectedTab={nameof(HomePage)}");
+        }
+    }
+}
